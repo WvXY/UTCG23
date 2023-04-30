beca7b1fbb8440b5c954c7ba648b968a1703e549
XY.Wu <wooxyu@gmail.com>
2023-04-30 14:13:48 +0900

    add shader

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -1,21 +1,64 @@
-//
-// ここから開始
-//
-function main() {
-    const canvas = document.querySelector("#glCanvas");
-    // GL コンテキストを初期化する
-    const gl = canvas.getContext("webgl");
+const canvas = document.getElementById("glCanvas");
+// GL コンテキストを初期化する
+const gl = canvas.getContext("webgl");
 
-    // WebGL が使用可能で動作している場合にのみ続行します
-    if (gl === null) {
-        alert("WebGL を初期化できません。ブラウザーまたはマシンが対応していない可能性があります。");
-        return;
-    }
+let vertices = [
+    -0.7, -0.1, 0,
+    -0.3, 0.6, 0,
+    -0.3, -0.3, 0,
+    0.2, 0.6, 0,
+    0.3, -0.3, 0,
+    0.7, 0.6, 0
+    ];
 
-    // クリアカラーを黒に設定し、完全に不透明にします
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    // 指定されたクリアカラーでカラーバッファーをクリアします
-    gl.clear(gl.COLOR_BUFFER_BIT);
-}
+// Buffer
+const vertexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-window.onload = main;
+// Shader
+const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+// https://www.tutorialspoint.com/webgl/webgl_modes_of_drawing.htm#
+const vertexSource =
+    'attribute vec3 coordinates;' +
+    'void main(void) {' +
+    'gl_Position = vec4(coordinates, 1.0);' +
+    '}';
+
+
+gl.shaderSource(vertexShader, vertexSource);
+gl.compileShader(vertexShader);
+
+
+const fragmentSource =
+    'void main(void) {' +
+    'gl_FragColor = vec4(0.0, 0.0, 1.0, 1);' +
+    '}';
+
+gl.shaderSource(fragmentShader, fragmentSource);
+gl.compileShader(fragmentShader);
+
+// Shader Program
+const shaderProgram = gl.createProgram();
+gl.attachShader(shaderProgram,vertexShader);
+gl.attachShader(shaderProgram,fragmentShader);
+gl.linkProgram(shaderProgram);
+gl.useProgram(shaderProgram);
+
+console.log(gl)
+
+gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+const coord = gl.getAttribLocation(shaderProgram, "coordinates");
+gl.vertexAttribPointer(coord, 3, gl.FLOAT, false, 0, 0); // what?
+gl.enableVertexAttribArray(coord);
+
+// Draw
+//gl.clearColor(0,1, 1, 1);
+//gl.enable(gl.DEPTH_TEST);
+//gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+// gl.viewport(0,0,canvas.width, canvas.height);
+gl.drawArrays(gl.LINE_LOOP, 0, 6);
+
